2d0eee96f96ecab1122e113259015fb0687042a1
Christoph Ladurner <christoph.ladurner@tugraz.at>
2024-11-30 23:09:00 +0100

    fix: SphinxWarning
    * SphinxWarning("The pre-Sphinx 1.0 'intersphinx_mapping' format is
      deprecated and will be removed in Sphinx 8. Update to the current format
      as described in the documentation. Hint: `intersphinx_mapping =
      {'<name>': ('https://docs.python.org/',
      None)}`.https://www.sphinx-doc.org/en/master/usage/extensions/intersphinx.html#confval-intersphinx_mapping")

--- docs/conf.py
+++ docs/conf.py
@@ -2,7 +2,7 @@
 #
 # This file is part of Invenio.
 # Copyright (C) 2015-2018 CERN.
-# Copyright (C) 2022 Graz University of Technology.
+# Copyright (C) 2022-2024 Graz University of Technology.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -10,10 +10,6 @@
 """Sphinx configuration."""
 
 from __future__ import print_function
-
-import sys
-
-import sphinx.environment
 
 from invenio_jsonschemas import __version__
 
@@ -335,7 +331,9 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {"https://docs.python.org/": None}
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/", None),
+}
 
 # Autodoc configuraton.
 autoclass_content = "both"
